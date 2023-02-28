b866f77cc0ff3e85f7892b7ca432c793ee73423a
Anna Breck <annallucille@infra04-wg012.eduroam.wireless.umn.edu>
2023-02-27 18:47:09 -0600

    lab05 finished

--- lab/lab05/Matrix.java
+++ lab/lab05/Matrix.java
@@ -6,10 +6,12 @@ public class Matrix {
 
     public Matrix(int i, int j){
         nrows = i; ncols = j; 
-        matrix  = new int[i-1][j-1];
+        matrix  = new int[i][j];
     }
 
     public Matrix(int[][] arr){
+        nrows = arr.length; ncols = arr[0].length;
+        matrix = new int[nrows][ncols];
         for(int i = 0; i<=nrows-1; i++){
             for(int j = 0; j<= ncols-1; j++)
                 matrix[i][j] = arr[i][j];
@@ -17,8 +19,8 @@ public class Matrix {
     }
     public Matrix transpose(){
         Matrix newMatrix = new Matrix(nrows,ncols);
-        for(int i = 0; i<=nrows-1; i++){
-            for(int j = 0; j<= ncols-1; j++)
+        for(int i = 0; i<=nrows-2; i++){
+            for(int j = 0; j<= ncols-2; j++)
                 newMatrix.matrix[j][i] = matrix[i][j];
         }
         return newMatrix;
@@ -28,7 +30,7 @@ public class Matrix {
         String s = "\0";
         for(int i = 0; i<=nrows-1; i++){
             for(int j = 0; j<= ncols-1; j++){
-                s += String.valueOf((int)matrix[i][j]);
+                s += String.valueOf((int)matrix[i][j]) + " ";
             }
             s += "\n";
         }
@@ -36,10 +38,9 @@ public class Matrix {
     }
 
     public static void main(String[] args) {
-        Matrix matrixA = new Matrix(4,4);
         int[][] array = {{1,1,1,1,1},{0,1,2,3,4},{0,1,4,9,16},{0,1,8,27,64}};
 
-        matrixA = new Matrix(array);
+        Matrix matrixA = new Matrix(array);
         Matrix transA = matrixA.transpose();
 
         System.out.println(matrixA);
